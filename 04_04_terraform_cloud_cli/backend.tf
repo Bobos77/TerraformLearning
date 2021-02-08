b1a62a276e60d02bad6fe9ed03c6603515e5609b
Anton Push <app1977h@gmail.com>
2021-02-08 10:34:42 -0500

    set env tag staging

--- 04_04_terraform_cloud_cli/backend.tf
+++ 04_04_terraform_cloud_cli/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "red30"
+    organization = "AntonPushkarev"
 
     workspaces {
-      name = "cli-workspace"
+      name = "TerraformLearning"
     }
   }
 }
